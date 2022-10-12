a24f3ae20f050672a00891af6d3fe95f79f64709
FaqWam <114764212+FaqWam@users.noreply.github.com>
2022-10-12 20:53:46 +0300

    19 Задача

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите целое число: ");
+string number = Console.ReadLine();
+int length = number.Length;
+string txtToCheck = number.Replace(" "," ");  
+
+bool IsPalindrome()
+{
+    for (int i = 0; i < length / 2; i++)
+        while (number[i] == number[length - 1])
+        {
+            return true;
+        }
+    return false;
+}
+
+if (IsPalindrome()) System.Console.WriteLine("Палиндром");
+else Console.Write("Не палиндром");
